83373b8d6a26a8c1ce48f2c59d11fe177fc63380
AndreyDuda <AndreyDuda88@gmail.com>
2020-03-18 00:57:47 +0200

    remove slim and install symfony 4.4.9

--- manager/public/index.php
+++ manager/public/index.php
@@ -1,27 +1,27 @@
 <?php
-declare(strict_types=1);
 
-use Slim\Http\Request;
-use Slim\Http\Response;
+use App\Kernel;
+use Symfony\Component\ErrorHandler\Debug;
+use Symfony\Component\HttpFoundation\Request;
 
-chdir(dirname(__DIR__));
-require 'vendor/autoload.php';
+require dirname(__DIR__).'/config/bootstrap.php';
 
-http_response_code(500);
+if ($_SERVER['APP_DEBUG']) {
+    umask(0000);
 
-(function () {
-    $app = new \Slim\App([
-        'settings' => [
-            'addContentLengthHeader' => false,
-            'displayErrorDetails' => (bool)getenv('APP_DEBUG'),
-        ],
-    ]);
+    Debug::enable();
+}
 
-    $app->get('/', function (Request $request, Response $response) {
-        return $response->withJson([
-            'name' => 'Manager',
-            'param' => $request->getQueryParam('param')
-        ]);
-    });
-    $app->run();
-})();
+if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
+    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
+}
+
+if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false) {
+    Request::setTrustedHosts([$trustedHosts]);
+}
+
+$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
