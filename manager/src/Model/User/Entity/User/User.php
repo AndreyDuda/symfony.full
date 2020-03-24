b22685c23537c5b66236db77642241fab7a5aa06
AndreyDuda <AndreyDuda88@gmail.com>
2020-03-24 23:26:22 +0200

    rewrite logic user and add test v1

---
+++ manager/src/Model/User/Entity/User/User.php
@@ -0,0 +1,43 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Model\User\Entity\User;
+
+class User
+{
+    /** @var string */
+    private $id;
+    /** @var \DateTimeImmutable  */
+    private $date;
+    /** @var string  */
+    private $email;
+    /** @var string  */
+    private $passwordHash;
+
+    public function __construct(string $id, \DateTimeImmutable $date, string $email, string $hash)
+    {
+        $this->id = $id;
+        $this->date = $date;
+        $this->email = $email;
+        $this->passwordHash = $hash;
+    }
+
+    public function getId(): string
+    {
+       return $this->id;
+    }
+
+    public function getDate(): \DateTimeImmutable
+    {
+        return $this->date;
+    }
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function getPasswordHash(): string
+    {
+        return $this->passwordHash;
+    }
+}
---
+++ manager/src/Model/User/UseCase/SignUp/Request/Handler.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Model\User\UserCase\SignUp\Request;
+
+use App\Model\User\Entity\User\User;
+use Doctrine\ORM\EntityManager;
+use Ramsey\Uuid\Uuid;
+
+class Handler
+{
+    /**
+     * @var EntityManager
+     */
+    private $em;
+
+    public function __construct(EntityManager $em)
+    {
+        $this->em = $em;
+    }
+
+    public function handler(Command $command): void
+    {
+        $email = mb_strtolower($command->email);
+
+        if ($this->em->getRepository(User::class)->findOneBy(['email' => $email])) {
+            throw new \DomainException('User already exist.');
+        }
+
+        $user = new User(
+            Uuid::uuid4()->toString(),
+            new \DateTimeImmutable(),
+            $email,
+            password_hash($command->password, PASSWORD_ARGON2I)
+        );
+
+        $this->em->persist($user);
+        $this->em->flush();
+    }
+}
