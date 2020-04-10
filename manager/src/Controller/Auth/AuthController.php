f18f3d97c201c5f0cdedc2e3c8540853a04294a6
AndreyDuda <AndreyDuda88@gmail.com>
2020-04-10 23:04:52 +0300

    add mailhog and auth

---
+++ manager/src/Controller/Auth/AuthController.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller\Auth;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class AuthController extends AbstractController
+{
+    /**
+     * @Route("/login", name="app_login")
+     * @param AuthenticationUtils $authenticationUtils
+     * @return Response
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('app/auth/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+}
